8e7265fa0baa1f7a1ffb70b209d8d3c5c564729f
Joey Chen <jay.hatelove@gmail.com>
2022-10-08 22:01:56 +0800

    fix: period_no_overlap_after_budget

--- BudgetTest/Period.cs
+++ BudgetTest/Period.cs
@@ -19,6 +19,11 @@ public class Period
 
     public int OverlappingDays(Period another)
     {
+        if (Start > another.End)
+        {
+            return 0;
+        }
+
         if (End < another.Start)
         {
             return 0;
