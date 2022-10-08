4e26d39d4b4014ff7bbb048a149f66271cef706a
Joey Chen <jay.hatelove@gmail.com>
2022-10-08 19:43:20 +0800

    fix: middle months while loop condition bug

--- BudgetTest/BudgetService.cs
+++ BudgetTest/BudgetService.cs
@@ -1,6 +1,9 @@
+#region
+
 using System;
-using System.Collections.Generic;
 using System.Linq;
+
+#endregion
 
 namespace BudgetTest;
 
@@ -20,54 +23,34 @@ public class BudgetService
             var result = 0m;
             var currentStart = start.AddMonths(1);
 
-            while (currentStart < end)
+            var loopStopCondition = new DateTime(end.Year, end.Month, 1);
+            while (currentStart < loopStopCondition)
             {
                 var monthBudget = GetMonthBudget(currentStart);
                 result += monthBudget.Amount;
                 currentStart = currentStart.AddMonths(1);
             }
-            
+
             var budgetStart = GetMonthBudget(start);
             var budgetEnd = GetMonthBudget(end);
-            
-            var startBudgetPerDay = GetBudgetPerDay(start , budgetStart.Amount);
-            var endBudgetPerDay = GetBudgetPerDay(end , budgetEnd.Amount);
-            
-            var dayDiffStart = GetDayDiff(start, new DateTime(start.Year,start.Month,DateTime.DaysInMonth(start.Year,start.Month)));
-            var dayDiffEnd = GetDayDiff(new DateTime(end.Year,end.Month,01), end);
-            
-            var startAmount = CalculateAmount(dayDiffStart , startBudgetPerDay);
-            var endAmount = CalculateAmount(dayDiffEnd , endBudgetPerDay);
-            
+
+            var startBudgetPerDay = GetBudgetPerDay(start, budgetStart.Amount);
+            var endBudgetPerDay = GetBudgetPerDay(end, budgetEnd.Amount);
+
+            var dayDiffStart = GetDayDiff(start, new DateTime(start.Year, start.Month, DateTime.DaysInMonth(start.Year, start.Month)));
+            var dayDiffEnd = GetDayDiff(new DateTime(end.Year, end.Month, 01), end);
+
+            var startAmount = CalculateAmount(dayDiffStart, startBudgetPerDay);
+            var endAmount = CalculateAmount(dayDiffEnd, endBudgetPerDay);
+
             var amount = startAmount + endAmount;
             return result + amount;
-        }   
+        }
+
         var budget = GetMonthBudget(start);
         var budgetPerDay = GetBudgetPerDay(start, budget.Amount);
         var diffDays = GetDayDiff(start, end);
         return CalculateAmount(diffDays, budgetPerDay);
-    }
-
-    private Budget GetMonthBudget(DateTime date)
-    {
-        var budgets = _budgetRepo.GetAll();
-        var budget = budgets.FirstOrDefault(x => x.YearMonth == $"{date:yyyyMM}");
-        if (budget == null)
-        {
-            return new Budget
-            {
-                YearMonth = date.ToString("yyyyMM"),
-                Amount = 0
-            };
-        }
-
-        return budget;
-    }
-
-    private static int GetDayDiff(DateTime start, DateTime end)
-    {
-        var diffDays = (end - start).Days + 1;
-        return diffDays;
     }
 
     private static decimal CalculateAmount(int diffDays, int budgetPerDay)
@@ -78,5 +61,27 @@ public class BudgetService
     private static int GetBudgetPerDay(DateTime date, int amount)
     {
         return amount / DateTime.DaysInMonth(date.Year, date.Month);
+    }
+
+    private static int GetDayDiff(DateTime start, DateTime end)
+    {
+        var diffDays = (end - start).Days + 1;
+        return diffDays;
+    }
+
+    private Budget GetMonthBudget(DateTime date)
+    {
+        var budgets = _budgetRepo.GetAll();
+        var budget = budgets.FirstOrDefault(x => x.YearMonth == $"{date:yyyyMM}");
+        if (budget == null)
+        {
+            return new Budget
+                   {
+                       YearMonth = date.ToString("yyyyMM"),
+                       Amount = 0
+                   };
+        }
+
+        return budget;
     }
 }
--- BudgetTest/BudgetTest.cs
+++ BudgetTest/BudgetTest.cs
@@ -1,15 +1,19 @@
+#region
+
 using System;
 using System.Collections.Generic;
 using FluentAssertions;
 using NSubstitute;
 using Xunit;
 
+#endregion
+
 namespace BudgetTest;
 
 public class BudgetTest
 {
-    private IBudgetRepo _budgetRepo;
-    private BudgetService _budgetService;
+    private readonly IBudgetRepo _budgetRepo;
+    private readonly BudgetService _budgetService;
 
     public BudgetTest()
     {
@@ -17,37 +21,17 @@ public class BudgetTest
         _budgetService = new BudgetService(_budgetRepo);
     }
 
-    private void GivenBudgets(List<Budget> budgets)
-    {
-        _budgetRepo.GetAll().Returns(budgets);
-    }
-
-    [Fact]
-    public void Partial_Month()
-    {
-        GivenBudgets(new List<Budget>()
-        {
-            new()
-            {
-                YearMonth = "202210",
-                Amount = 3100
-            }
-        });
-        var result = _budgetService.Query(new DateTime(2022, 10, 01), new DateTime(2022, 10, 02));
-        result.Should().Be(200m);
-    }
-
     [Fact]
     public void All_Month()
     {
         GivenBudgets(new List<Budget>()
-        {
-            new()
-            {
-                YearMonth = "202210",
-                Amount = 3100
-            }
-        });
+                     {
+                         new()
+                         {
+                             YearMonth = "202210",
+                             Amount = 3100
+                         }
+                     });
         var result = _budgetService.Query(new DateTime(2022, 10, 01), new DateTime(2022, 10, 31));
         result.Should().Be(3100m);
     }
@@ -56,31 +40,68 @@ public class BudgetTest
     public void Cross_A_Month()
     {
         GivenBudgets(new List<Budget>()
-        {
-            new()
-            {
-                YearMonth = "202210",
-                Amount = 3100
-            },
-            new(yearMonth: "202211", amount: 300)
-        });
+                     {
+                         new()
+                         {
+                             YearMonth = "202210",
+                             Amount = 3100
+                         },
+                         new(yearMonth: "202211", amount: 300)
+                     });
         var result = _budgetService.Query(new DateTime(2022, 10, 31), new DateTime(2022, 11, 5));
         result.Should().Be(150m);
     }
+
     [Fact]
     public void Cross_Over_Two_Month()
     {
         GivenBudgets(new List<Budget>()
-        {
-            new()
-            {
-                YearMonth = "202210",
-                Amount = 3100
-            },
-            new(yearMonth: "202211", amount: 300),
-            new(yearMonth: "202212", amount:31),
-        });
+                     {
+                         new()
+                         {
+                             YearMonth = "202210",
+                             Amount = 3100
+                         },
+                         new(yearMonth: "202211", amount: 300),
+                         new(yearMonth: "202212", amount: 31),
+                     });
         var result = _budgetService.Query(new DateTime(2022, 10, 31), new DateTime(2022, 12, 3));
-        result.Should().Be(100m+300m+3);
+        result.Should().Be(100m + 300m + 3);
+    }
+
+    [Fact]
+    public void day_of_start_less_than_day_of_end_when_cross_3_months()
+    {
+        GivenBudgets(new List<Budget>()
+                     {
+                         new(yearMonth: "202210", amount: 3100),
+                         new(yearMonth: "202211", amount: 300),
+                         new(yearMonth: "202212", amount: 31),
+                     });
+        var result = _budgetService.Query(
+            new DateTime(2022, 10, 1),
+            new DateTime(2022, 12, 2));
+
+        result.Should().Be(3100 + 300 + 2);
+    }
+
+    [Fact]
+    public void Partial_Month()
+    {
+        GivenBudgets(new List<Budget>()
+                     {
+                         new()
+                         {
+                             YearMonth = "202210",
+                             Amount = 3100
+                         }
+                     });
+        var result = _budgetService.Query(new DateTime(2022, 10, 01), new DateTime(2022, 10, 02));
+        result.Should().Be(200m);
+    }
+
+    private void GivenBudgets(List<Budget> budgets)
+    {
+        _budgetRepo.GetAll().Returns(budgets);
     }
 }
