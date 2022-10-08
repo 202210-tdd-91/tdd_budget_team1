cf57569905933310d3bc257375141ce17f64a653
Joey Chen <jay.hatelove@gmail.com>
2022-10-08 22:07:09 +0800

    clean up Budget class

--- BudgetTest/Budget.cs
+++ BudgetTest/Budget.cs
@@ -21,30 +21,30 @@ public class Budget
     public int Amount { get; set; }
     public string YearMonth { get; set; } = default!;
 
-    public DateTime GetFirstDay()
+    public decimal OverlappingAmount(Period period)
     {
-        return DateTime.ParseExact(YearMonth, "yyyyMM", null);
+        return period.OverlappingDays(CreatePeriod()) * DailyAmount();
     }
 
-    public DateTime GetLastDay()
-    {
-        var firstDay = GetFirstDay();
-        var daysInMonth = DateTime.DaysInMonth(firstDay.Year, firstDay.Month);
-        return new DateTime(firstDay.Year, firstDay.Month, daysInMonth);
-    }
-
-    public decimal DailyAmount()
-    {
-        return Amount / (decimal)GetLastDay().Day;
-    }
-
-    public Period CreatePeriod()
+    private Period CreatePeriod()
     {
         return new Period(GetFirstDay(), GetLastDay());
     }
 
-    public decimal OverlappingAmount(Period period)
+    private decimal DailyAmount()
     {
-        return period.OverlappingDays(CreatePeriod()) * DailyAmount();
+        return Amount / (decimal)GetLastDay().Day;
+    }
+
+    private DateTime GetFirstDay()
+    {
+        return DateTime.ParseExact(YearMonth, "yyyyMM", null);
+    }
+
+    private DateTime GetLastDay()
+    {
+        var firstDay = GetFirstDay();
+        var daysInMonth = DateTime.DaysInMonth(firstDay.Year, firstDay.Month);
+        return new DateTime(firstDay.Year, firstDay.Month, daysInMonth);
     }
 }
